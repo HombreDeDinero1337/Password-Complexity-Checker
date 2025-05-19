d6cae51de33918800d28690180bfcada831849bf
moneyman <adil.khan2008@yahoo.com>
2025-05-19 01:11:25 -0400

    Update Password_Checker.py

--- Password_Checker.py
+++ Password_Checker.py
@@ -1,7 +1,15 @@
-# Get user's password
+# Get the user's password
 pass = input("Enter your password.")
 
 def main():
-  # Check for uppercase, lowercase, number, and a special character
+  # Assign variables for each requirement
+  upper_chars = False
+  lower_chars = False
+  digits = False
+  special = False
+
+  # Make a list of special characters to reference to
+  special_chars = "!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~"
+  
+  # Check for uppercase, lowercase, numbers, and a special character
   for char in pass
-    
